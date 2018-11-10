b7a7c190657c43ec53048f973379f6c290c3fe96
Shannon Capper <ShannonLCapper@gmail.com>
2018-11-10 00:01:18 -0800

    Add linter

---
+++ demo-plugin/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "textio-base/react"
+  ],
+  settings: {},
+  env: {
+    browser: true,
+    webextensions: true,
+  }
+}
